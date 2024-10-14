fc1d7163e007ecf91a969e16734e8eabb970ad5e
pumpkin <78476507+tessyoun@users.noreply.github.com>
2024-10-14 18:19:38 +0900

    [Silver III] Title: N과 M (3), Time: 1684 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+answer =[]
+
+def dfs():
+    if len(answer) == M:
+        print(*answer)
+        return
+        
+    for i in range(1, N+1):
+        answer.append(i)
+        dfs()
+        answer.pop()
+        
+dfs()
