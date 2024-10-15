57bbb5b1745d4db286c2fe221d4376cd22e8aba2
pumpkin <78476507+tessyoun@users.noreply.github.com>
2024-10-15 23:03:11 +0900

    [Silver II] Title: N과 M (9), Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,23 +1,23 @@
-from collections import Counter
-
-N, M = map(int, input().split())
-nums = list(map(int, input().split()))
-nums.sort()  # 순차적으로 출력하기 위해 정렬
-count = Counter(nums)  # 숫자들의 등장 횟수를 셈
-
-answer = []
+import sys
+input = sys.stdin.readline
 
 def dfs():
-    if len(answer) == M:  # 길이가 M인 조합을 완성했을 때 출력
-        print(*answer)
+    if len(temp) == m:
+        print(*temp)
         return
-    
-    for num in count:  # 등장한 각 숫자에 대해 반복
-        if count[num] > 0:  # 아직 사용할 수 있는 개수가 남아 있을 때
-            answer.append(num)
-            count[num] -= 1  # 사용했으니 하나 줄임
-            dfs()  # 재귀 호출
-            count[num] += 1  # 다시 복구 (백트래킹)
-            answer.pop()
+    remember = 0
+    for i in range(n):
+        if not visited[i] and remember != nums[i]:
+            visited[i] = True
+            temp.append(nums[i])
+            remember = nums[i]
+            dfs()
+            visited[i] = False
+            temp.pop()
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+visited = [False] * n
+temp = []
 
 dfs()
