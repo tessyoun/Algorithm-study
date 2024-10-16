16df08b60fa62563f4e933135037e93ab86dbc0c
pumpkin <78476507+tessyoun@users.noreply.github.com>
2024-10-16 15:36:07 +0900

    [Gold IV] Title: 월드컵, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.py
+++ 백준/Gold/6987. 월드컵/월드컵.py
@@ -1,25 +1,22 @@
-
 from itertools import combinations
 
 def check(round):
-    global answer
     if round == 15:
-        answer = 1
         for i in score_list:
             if sum(i) != 0:
-                answer = 0
-                break
-        return    
+                return False
+        return True 
         
-    
     con1, con2 = game[round]
     for x, y in ((0,2), (1,1), (2,0)):
         if score_list[con1][x] > 0 and score_list[con2][y] > 0:
             score_list[con1][x] -= 1
             score_list[con2][y] -= 1
-            check(round+1)
+            if check(round + 1): return True
             score_list[con1][x] += 1
-            score_list[con2][y] += 1        
+            score_list[con2][y] += 1       
+            
+    return False 
 
 result = []
 game = list(combinations(range(6), 2))
@@ -27,9 +24,7 @@ game = list(combinations(range(6), 2))
 for _ in range(4):
     temp = list(map(int, input().split()))
     score_list = [temp[i:i+3] for i in range(0, 16, 3)]
-    answer = 0
-    check(0)
-    result.append(answer)
+    if check(0): result.append(1)
+    else: result.append(0)
     
 print(*result)
-
