a8af3ce5dba1a141aa1c4ee14d27740fff8b0352
pumpkin <78476507+tessyoun@users.noreply.github.com>
2024-10-19 21:56:14 +0900

    [Gold IV] Title: 좋은수열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,28 +1,31 @@
 import sys
 
-def is_goodSeq(temp, l):
-    for i in range(l-2):
-        cand = temp[i]
-        for j in range(i+1, l-1):
-            if temp[j] == cand and j+j-i <= l:
-                if temp[i:j] == temp[j:j+j-i]: return False
+# 부분 수열이 반복되는지 체크하는 함수
+def is_goodSeq(temp):
+    l = len(temp)
+    # 부분 수열이 반복되는지 확인
+    for length in range(1, l // 2 + 1):
+        if temp[-length:] == temp[-2*length:-length]:
+            return False
     return True
 
-def dfs():      
-    if len(temp) > 3:
-        if not is_goodSeq(temp, len(temp)): return
-                  
+def dfs():
+    # 길이가 N이면 수열 출력 후 종료
     if len(temp) == N:
-        print(*temp, sep='')
-        sys.exit()
-        return 
-    
+        print(*temp, sep='')  # 띄어쓰기 없이 출력
+        sys.exit()  # 프로그램 종료
+        return
+
+    # 1부터 3까지 수열 생성
     for i in range(1, 4):
+        # 이전에 넣은 숫자와 현재 넣을 숫자가 다를 때만 추가
         if not temp or temp[-1] != i:
             temp.append(i)
-            dfs()
+            # 부분 수열이 유효한지 확인
+            if is_goodSeq(temp):
+                dfs()
             temp.pop()
-    
+
 N = int(input())
 temp = []
 dfs()
