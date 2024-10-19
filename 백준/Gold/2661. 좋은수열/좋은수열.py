a2ade5da441b0ef57b78c43fbdad823042a7c6fd
pumpkin <78476507+tessyoun@users.noreply.github.com>
2024-10-19 22:04:02 +0900

    [Gold IV] Title: 좋은수열, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,31 +1,26 @@
 import sys
 
-# 부분 수열이 반복되는지 체크하는 함수
-def is_goodSeq(temp):
-    l = len(temp)
-    # 부분 수열이 반복되는지 확인
+def is_goodSeq(temp, l):
     for length in range(1, l // 2 + 1):
         if temp[-length:] == temp[-2*length:-length]:
             return False
     return True
 
-def dfs():
-    # 길이가 N이면 수열 출력 후 종료
+def dfs():      
+    if len(temp) > 3:
+        if not is_goodSeq(temp, len(temp)): return
+                  
     if len(temp) == N:
-        print(*temp, sep='')  # 띄어쓰기 없이 출력
-        sys.exit()  # 프로그램 종료
-        return
-
-    # 1부터 3까지 수열 생성
+        print(*temp, sep='')
+        sys.exit()
+        return 
+    
     for i in range(1, 4):
-        # 이전에 넣은 숫자와 현재 넣을 숫자가 다를 때만 추가
         if not temp or temp[-1] != i:
             temp.append(i)
-            # 부분 수열이 유효한지 확인
-            if is_goodSeq(temp):
-                dfs()
+            dfs()
             temp.pop()
-
+    
 N = int(input())
 temp = []
 dfs()
